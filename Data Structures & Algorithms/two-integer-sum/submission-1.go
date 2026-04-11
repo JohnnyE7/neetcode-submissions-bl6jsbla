93af41f55b9921fcaef0044887bd588742b5dc0e
Eugene <152658271+JohnnyE7@users.noreply.github.com>
2026-04-11 14:05:16 +0300

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -0,0 +1,19 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int, len(nums))
+	var res []int
+
+    for i, num := range nums {
+		need := target - num
+		j, ok := m[need]
+		if ok {
+			if i < j {
+				res = []int{i, j}
+			} else {
+				res = []int{j, i}
+			}
+			return res
+		}
+		m[num] = i
+	}
+	return res
+}
