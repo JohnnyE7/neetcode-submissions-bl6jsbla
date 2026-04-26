b2aa5daf3802d7577c836c4f2fd4a8c03dc42381
Eugene <152658271+JohnnyE7@users.noreply.github.com>
2026-04-26 04:56:38 +0300

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.go
@@ -0,0 +1,40 @@
+type MinStack struct {
+	data []int
+	mins []int
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+	this.data = append(this.data, val)
+
+	if len(this.mins) == 0 || val <= this.mins[len(this.mins) - 1] {
+		this.mins = append(this.mins, val)
+	}
+}
+
+func (this *MinStack) Pop() {
+	minIdx := len(this.mins) - 1
+	if this.Top() == this.minTop() {
+		this.mins = this.mins[:minIdx]
+	}
+
+	lastIdx := len(this.data) - 1
+	this.data = this.data[:lastIdx]
+}
+
+func (this *MinStack) Top() int {
+	lastIndex := len(this.data) - 1
+	return this.data[lastIndex]
+}
+
+func (this *MinStack) minTop() int {
+	lastIndex := len(this.mins) - 1
+	return this.mins[lastIndex]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minTop()
+}
